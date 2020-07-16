1c913b9681c09823ba28aee49780c133d672ae7c
Shaun McGuigan <Shaun@Goldenrod.local>
2020-07-16 10:43:23 -0700

    Add all initial files

---
+++ dh_hw.cpp
@@ -0,0 +1,45 @@
+#include "systemc.h"
+#include "digit.h"
+#include "dh_hw.h"
+
+void dh_hw::process_hw()
+{
+
+  NN_DIGIT t[2], c, u, v;
+  NN_HALF_DIGIT aLow, cLow, cHigh;
+  
+  for (;;) {  
+  
+      // Read inputs (blocking FIFO access)
+      t[0] = from_sw0.read();
+      t[1] = from_sw1.read();
+      c = from_sw2.read();
+      aLow = from_sw3.read();
+
+		
+	    // Original code from NN_DigitDivLH():	
+
+      /*** Required part (to do: Datapath + Control) ***/
+      cHigh = (NN_HALF_DIGIT)HIGH_HALF (c);
+      cLow = (NN_HALF_DIGIT)LOW_HALF (c);
+  
+      u = (NN_DIGIT)aLow * (NN_DIGIT)cLow;
+      v = (NN_DIGIT)aLow * (NN_DIGIT)cHigh;
+  
+      if ((t[0] -= u) > (MAX_NN_DIGIT - u)) t[1]--;
+      if ((t[0] -= TO_HIGH_HALF (v)) > (MAX_NN_DIGIT - TO_HIGH_HALF (v))) t[1]--;
+      t[1] -= HIGH_HALF (v);
+
+      /*** Bonus part (optional: Extra Datapath + Extra Control) ***/
+      while ((t[1] > 0) || ((t[1] == 0) && t[0] >= c)) {
+        if ((t[0] -= c) > (MAX_NN_DIGIT - c)) t[1]--;
+        aLow++;
+      }
+      
+		
+      // Write outputs (blocking FIFO access)
+      to_sw.write(aLow);
+
+  }
+	  	  
+}
