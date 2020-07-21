87afa3772e8b3f6b26750caf12e5d4d95033312d
Shaun McGuigan <Shaun@Goldenrod.local>
2020-07-21 11:12:46 -0700

    Move implementation of bonus module into header file

--- bonus.h
+++ bonus.h
@@ -5,17 +5,32 @@
 #include "digit.h"
 
 SC_MODULE(bonus) {
-  // Ports
   sc_in<NN_DIGIT> t0_new_sig, t1_new_sig, c_sig;
   sc_in<NN_HALF_DIGIT> aLow_sig;
   sc_in<bool> ready_sig;
   sc_out<NN_HALF_DIGIT> aLow_new_sig;
   
-  // Constructor
-  bonus(sc_module_name);
+  SC_CTOR(bonus) {
+    SC_METHOD(bonus_method);
+    sensitive << ready_sig;
+  }
   
-  // Processes
-  void bonus_method();
+  void bonus_method() {
+    if (ready_sig->read() == true) {
+      NN_DIGIT t0_new(t0_new_sig->read());
+      NN_DIGIT t1_new(t1_new_sig->read());
+      NN_DIGIT c(c_sig->read());
+      NN_HALF_DIGIT aLow(aLow_sig->read());
+    
+      while ((t1_new > 0) || ((t1_new == 0) && (t0_new >= c))) {
+        if ((t0_new -= c) > (MAX_NN_DIGIT - c)) {
+          t1_new--;
+        }
+        aLow++;
+      }
+      aLow_new_sig->write(aLow);
+    }
+  }
 };
 
 #endif // BONUS_H
